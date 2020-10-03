f8d63d94d204fe1f54dd948ce435c6e2f3d4ba99
Mikhail Hursky <kakai747o@gmail.com>
2020-10-03 18:30:23 +0300

    feat: add func check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    str = [...str]
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if ((bracketsConfig[j][0] === str[i]) && (bracketsConfig[j][1] === str[i + 1])) {
+                str.splice(i, 2)
+                i -= 2
+            }
+        }
+    }
+    return str.length === 0
 }
